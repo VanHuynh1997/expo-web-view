d1d5fc68345e665773cff5d6b643ef94ba3076e1
quangvan <van.huynhquang@haravan.com>
2024-08-08 13:46:27 +0700

    first commit1

---
+++ ios/ExpoWebViewModule.swift
@@ -0,0 +1,17 @@
+import ExpoModulesCore
+
+public class ExpoWebViewModule: Module {
+  public func definition() -> ModuleDefinition {
+    Name("ExpoWebView")
+
+    View(ExpoWebView.self) {
+      Events("onLoad")
+      Prop("url") { (view, url: URL) in
+        if view.webView.url != url {
+          let urlRequest = URLRequest(url: url)
+          view.webView.load(urlRequest)
+        }
+      }
+    }
+  }
+}
